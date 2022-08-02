fdd11d2f475767b2f08fb08a4e0a1d2b96b26dc4
sunny gangadhar koppala <sunnygangadharkoppala54@gmail.com>
2022-08-02 10:15:15 +0000

    commit three

--- store_quotes.py
+++ store_quotes.py
@@ -1,24 +1,65 @@
 import sqlite3
 
-connection_obj = sqlite3.connect('quotes.db')
+
+def createAuthorsTable():
+    connection_obj = sqlite3.connect('quotes.db')
+    cursor_obj = connection_obj.cursor()
+    cursor_obj.execute("DROP TABLE IF EXISTS AUTHORS")
+    authors_table = """ CREATE TABLE Authors (
+                id INT NOT NULL PRIMARY KEY,
+                name VARCHAR(500),
+                author_url VARCHAR(250)
+            ); """
+    cursor_obj.execute(authors_table)
+    print("Author Table is Ready")
+    connection_obj.close()
 
 
-cursor_obj = connection_obj.cursor()
+def createQuotesTable():
+    connection_obj = sqlite3.connect('quotes.db')
+    cursor_obj = connection_obj.cursor()
+    cursor_obj.execute("DROP TABLE IF EXISTS QUOTES")
+    quotes_table = """ CREATE TABLE Quotes (
+                id INT NOT NULL PRIMARY KEY,
+                quote TEXT,
+                author_id INT,
+                CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES Authors(id) ON DELETE CASCADE
+            ); """
+    cursor_obj.execute(quotes_table)
+    print("Quotes Table is Ready")
+    connection_obj.close()
 
 
-authors_table = """ CREATE TABLE Authors (
-			id INT NOT NULL PRIMARY KEY,
-			name VARCHAR(500),
-            author_url VARCHAR(250),
-		) """
-# quotes_table = """ CREATE TABLE Quotes (
-# 			id INT NOT NULL PRIMARY KEY,
-# 			quote TEXT,
-#             author_id INT,
-#             FOREGIN KEY (author_id) REFERENCES Authors(id) ON DELETE CASCADE
-# 		); """
-cursor_obj.execute(authors_table)
+def createTagsTable():
+    connection_obj = sqlite3.connect('quotes.db')
+    cursor_obj = connection_obj.cursor()
+    cursor_obj.execute("DROP TABLE IF EXISTS Tags")
+    tags_table = """ CREATE TABLE Tags (
+                id INT NOT NULL PRIMARY KEY,
+                category INT
+            ); """
+    cursor_obj.execute(tags_table)
+    print("Tags Table is Ready")
+    connection_obj.close()
 
-print("Table is Ready")
 
-connection_obj.close()
+def createQuotesAndTagsTable():
+    connection_obj = sqlite3.connect('quotes.db')
+    cursor_obj = connection_obj.cursor()
+    cursor_obj.execute("DROP TABLE IF EXISTS QUOTESANDTAGS")
+    quotesandtags_table = """ CREATE TABLE QuotesAndTags (
+                id INT NOT NULL PRIMARY KEY,
+                quote_id INT,
+                tag_id INT,
+                CONSTRAINT fk_quotes FOREIGN KEY (quote_id) REFERENCES Tags(id) ON DELETE CASCADE
+                CONSTRAINT fk_tags FOREIGN KEY (tag_id) REFERENCES Quotes(id) ON DELETE CASCADE
+            ); """
+    cursor_obj.execute(quotesandtags_table)
+    print("QuotesAndTags Table is Ready")
+    connection_obj.close()
+
+
+createAuthorsTable()
+createQuotesTable()
+createTagsTable()
+createQuotesAndTagsTable()
